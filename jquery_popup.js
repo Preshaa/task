e14df129e69c33fcb3a3351c91cc0ba502e9be68
preshaa <jainpreeti2409@gmail.com>
2017-01-02 13:14:12 +0530

    some_message

--- jquery_popup.js
+++ jquery_popup.js
@@ -1,4 +1,10 @@
-
+$(document).ready(function(){
+       $(".Popup").append("<button class='close' style='position:absolute; top:0px;right:0px;cursor:pointer;'>&times;</button>");
+       $(".Popup").hide();
+       $(".close").click(function(){
+           	  $(".Popup").Close();
+           });
+   });
 jQuery.fn.popup= function() {
     this.show();
 };
